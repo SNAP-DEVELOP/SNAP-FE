23a0d9bf56b9805c0aa0959902d739f3def60636
lamby-yang <18513512808@163.com>
2024-04-16 19:54:08 +0800

    Implement ESlint Configuration

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
-
-import { useLaunch } from '@tarojs/taro'
-import './app.css'
+import { useLaunch } from "@tarojs/taro";
+import "./app.css";
 
 function App({ children }) {
+    useLaunch(() => {
+        console.log("App launched.");
+    });
 
-  useLaunch(() => {
-    console.log('App launched.')
-  })
-
-  // children 是将要会渲染的页面
-  return children
+    // children 是将要会渲染的页面
+    return children;
 }
 
-export default App
+export default App;
+
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,16 +1,20 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.css'
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.css";
+import { AtButton } from "taro-ui";
 
 export default function Index() {
+    useLoad(() => {
+        console.log("Page loaded.");
+    });
 
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
-
-  return (
-    <View className='index'>
-      <Text>Hello world!</Text>
-    </View>
-  )
+    return (
+        <View className="index">
+            <Text>Hello world! !!</Text>
+            <AtButton type="primary" customStyle={{ border: "1px solid #000" }}>
+                按钮文案
+            </AtButton>
+        </View>
+    );
 }
+
