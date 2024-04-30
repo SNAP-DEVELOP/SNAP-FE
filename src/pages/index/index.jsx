4406fd6eba0b302d92049d239d4468e24a7d29e9
hanhan <zhangruohan200479@qq.com>
2024-04-30 15:41:12 +0800

    add photographer-sample card

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -14,10 +14,20 @@ export default function Index() {
             <View className="index.backgroundLogo"></View>
             <View className="index.banner"></View>
             <View className="index.card">
-                <View className="index.right"></View>
-
+                <View className="index.ArrowLeft"></View>
                 <View className="index.picture"></View>
-            </View>
+                <View className="index.ArrowRight"></View>
+                <View className="index.user"></View>
+                <View className="index.username"></View> 
+                <View className="index.number"></View>
+                <View className="index.heart"></View>
+                <View className="index.star"></View> 
+            </View>  
+            <View className="index.card2">
+                <View className="index.ArrowLeft2"></View>
+                <View className="index.picture2"></View>
+                <View className="index.ArrowRight2"></View>    
+            </View>  
         </View>
     );
 }
